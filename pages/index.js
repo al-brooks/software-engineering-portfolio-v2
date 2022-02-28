e2d46d3c200aeeae0b32e326cc842c275433b4fc
Alexander Brooks <alexanderlawbrooks@gmail.com>
2022-02-27 21:49:02 -0500

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,24 @@ export default function Home() {
     <Layout home>
       <Head>
         <title>Alexander Brooks</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <nav className="mb-5 flex flex-col text-center">
         <MenuCard option={"About Me"} url={"/about"} />
